c478980a394353b882977a790d6050b61f9a3da9
Harsh Maniar <harshmaniar@Mac.lan>
2025-02-20 12:56:22 +0530

    learn variables in goLang and public and private terminology

--- myutils/myutils.go
+++ myutils/myutils.go
@@ -1,10 +1,40 @@
 package myutils
 
 import "fmt"
-var Public string= "Public"
 
-func PrintMessage(){
-	name := "harsh"
-	fmt.Println(("Hello World "), name);
+// Global Variable (Public)
+var Public string = "Public Variable"
+
+// Global Variable (Private)
+var privateVar int = 42
+
+// Constant
+const Pi = 3.1415
+
+// Function demonstrating variable usage
+func PrintMessage() {
+	// Local Variable
+	var name string = "Harsh"
+	
+	// Short Variable Declaration (only inside functions)
+	age := 25
+
+	// Multiple Variable Declaration
+	var x, y int = 10, 20
+	country, city := "India", "Delhi"
+
+	// Default Zero Values
+	var emptyString string  // ""
+	var emptyInt int        // 0
+	var emptyBool bool      // false
+
+	// Print Statements
+	fmt.Println("Hello World,", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Public Variable:", Public)
+	fmt.Println("Private Variable:", privateVar)
+	fmt.Println("Pi Constant:", Pi)
+	fmt.Println("X and Y:", x, y)
+	fmt.Println("Country and City:", country, city)
+	fmt.Println("Default Values:", emptyString, emptyInt, emptyBool)
 }
-
