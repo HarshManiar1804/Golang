113f475a464f7d5c56fe21c04badf76876c07002
Harsh Maniar <harshmaniar@Mac.lan>
2025-02-20 14:01:01 +0530

    learn format specifiers

---
+++ print/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 18
+	name := "John"
+	height := 5.8543
+	fmt.Printf("My age is %d and name is %s and height is %.2f",age,name, height)
+	// fmt.Println("age",age,"name",name)
+}
