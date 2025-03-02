2f872caab47bf97107db90b5926a343fc2076f0d
HarshManiar1804 <harshmaniarofficial@gmail.com>
2025-03-02 12:46:54 +0530

    add POST in crud of golang

--- crud_api/main.go
+++ crud_api/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 type TODO struct{
@@ -11,9 +13,9 @@ type TODO struct{
 	Id int64 `json:"id"`
 	Title string `json:"title"`
 	Completed bool `json:"completed"`
-
 }
-func main(){
+
+func performGetRequest(){
 	res,err:=http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	// error handling 
 	if err != nil{
@@ -26,7 +28,41 @@ func main(){
 	// read the response
 	var todo TODO
 	_= json.NewDecoder(res.Body).Decode(&todo)
-
 	fmt.Println("data of first todo is ",todo)
+}
+func performPostRequest(){
+	myURL := "https://jsonplaceholder.typicode.com/todos/1"
+	todo:= TODO{
+		UserId :23,
+		Id :201,
+		Title :"newTitle",
+		Completed :false,
+	}
 
+	// convert todo structure into json format
+	todoJsonData,_:=json.Marshal(todo)
+
+	// convert json data to string
+	jsonString := string(todoJsonData)
+
+	// convert string data into reader
+	jsonReader:=strings.NewReader(jsonString)
+
+	// send POST request
+	res,err:=http.Post(myURL,"application/json",jsonReader)
+	if err != nil{
+		fmt.Println("posr req err")
+	}
+
+	defer res.Body.Close()
+	
+	data,_ :=io.ReadAll(res.Body)
+
+	fmt.Println("response is",string(data))
+	fmt.Println("response status",res.StatusCode)
+}
+func main(){
+	// performGetRequest()
+	//  performPostRequest()
+	
 }
